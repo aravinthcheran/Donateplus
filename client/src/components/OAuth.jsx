eee0da60bb20fe6c635e31b9bf34ca939b990655
aravinthcheran <cheran411@gmail.com>
2024-04-21 14:37:52 +0530

    Remove Donate- subproject

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,47 @@
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
+import { app } from '../firebase';
+import { useDispatch } from 'react-redux';
+import { signInSuccess } from '../redux/user/userSlice';
+import { useNavigate } from 'react-router-dom';
+import { FcGoogle } from "react-icons/fc";
+
+
+export default function OAuth() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const handleGoogleClick = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+
+      const result = await signInWithPopup(auth, provider);
+
+      const res = await fetch('/api/auth/google', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
+      const data = await res.json();
+      dispatch(signInSuccess(data));
+      navigate('/');
+    } catch (error) {
+      console.log('could not sign in with google', error);
+    }
+  };
+  return (
+    <button
+      onClick={handleGoogleClick}
+      type='button'
+      className='flex justify-center items-center bg-white text-black p-3 rounded-lg uppercase hover:opacity-95'
+    >
+  <span className="mr-2">Continue with Google</span>
+  <FcGoogle />
+</button>
+  );
+}
